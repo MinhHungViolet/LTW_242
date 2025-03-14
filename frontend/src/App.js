845081c27fbd38da58918e8b1ae721cee8956ad2
quocdat1964 <dat.nguyenquoc22itbk@hcmut.edu.vn>
2025-03-14 13:39:39 +0700

    Thêm trang thanh toán sản phẩm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import Header from './Layout/Header';
 import Footer from './Layout/Footer';
 import Navbar from './Layout/Navbar';
 import ProductList from './Product/ProductList';
+import Purchase from './Product/Purchase';
 
 function App() {
   return (
     <>
       <Header></Header>
       <Navbar></Navbar>
-      <ProductList></ProductList>
+      <Purchase></Purchase>
       
       <Footer></Footer>
     </>
