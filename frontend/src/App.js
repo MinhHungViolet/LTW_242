14cdfd976c4f3893e2f27696b6ca03b24c377cba
quochuy1962004 <voquochuy1962004@gmail.com>
2025-03-18 23:02:32 +0700

    create routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,18 @@ import './App.css';
 import Header from './Layout/Header';
 import Footer from './Layout/Footer';
 import Navbar from './Layout/Navbar';
-import ProductList from './Product/ProductList';
-import Purchase from './Product/Purchase';
-
+// import ProductList from './Product/ProductList';
+// import Purchase from './Product/Purchase';
+import ThemeRoutes from './Routes/index.js';
+import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header></Header>
       <Navbar></Navbar>
-      <Purchase></Purchase>
-      
+      <ThemeRoutes></ThemeRoutes>
       <Footer></Footer>
-    </>
+    </BrowserRouter>
   )
 }
 
